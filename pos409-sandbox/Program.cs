2abb696f8306ef79d82b872918a46b0f44e160e3
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-06-12 02:52:43 -0700

    add menu

--- pos409-sandbox/Program.cs
+++ pos409-sandbox/Program.cs
@@ -10,8 +10,34 @@ namespace pos409_sandbox
     {
         static void Main(string[] args)
         {
+            string input = "";
+
             Console.WriteLine("Welcome to the Simple Menu Demonstration Application");
             Console.WriteLine("====================================================");
+
+            while (!input.Equals("X"))
+            {
+                Console.WriteLine(Environment.NewLine + "Main Menu");
+                Console.WriteLine("=========");
+                Console.WriteLine("1: Greeting");
+                Console.WriteLine("2: Disclaimer");
+                Console.WriteLine("X: Exit");
+
+                input = Console.ReadLine();
+
+                if (input.Equals("1"))
+                {
+                    Console.WriteLine("Hello, World!");
+                }
+                else if (input.Equals("2"))
+                {
+                    Console.WriteLine("The author is not responsible for any damages, real or imagined, caused by this program.");
+                }
+                else if (input.Equals("X"))
+                {
+                    Console.WriteLine("Goodbye.");
+                }                
+            }
         }
     }
 }
