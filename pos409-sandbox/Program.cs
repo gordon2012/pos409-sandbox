e0b4cb462cfc7747b43f2dc7aee0ce7faca06827
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-06-18 08:22:45 -0700

    Real-World Case Scenario: Creating structs

--- pos409-sandbox/Program.cs
+++ pos409-sandbox/Program.cs
@@ -6,11 +6,86 @@ using System.Threading.Tasks;
 
 namespace pos409_sandbox
 {
+
+    // Struct
+    public struct bookStruct
+    {
+        public string title;
+        public string category;
+        public string author;
+        public int pages;
+        public int currentPage;
+        public string ISBN;
+        public string coverStyle;
+
+        public bookStruct(string title, string category, string author, int pages, string ISBN, string coverStyle)
+        {
+            this.title = title;
+            this.category = category;
+            this.author = author;
+            this.pages = pages < 1 ? 1 : pages;
+            this.currentPage = 1;
+            this.ISBN = ISBN;
+            this.coverStyle = coverStyle;
+        }
+
+        public void nextPage()
+        {
+            if (currentPage == pages )
+            {
+                Console.WriteLine("Already at the last page.");
+            }
+            else
+            {
+                currentPage++;
+                Console.WriteLine("Going ahead to page " + currentPage);
+            }
+        }
+
+        public void prevPage()
+        {
+            if (currentPage == 1)
+            {
+                Console.WriteLine("Already at the first page.");
+            }
+            else
+            {
+                currentPage++;
+                Console.WriteLine("Going back to page " + currentPage);
+            }
+        }
+
+        public void setPage(int page)
+        {
+            if (page >= 1 && page <= pages)
+            {
+                currentPage = page;
+                Console.WriteLine("Page set to " + currentPage);
+            }
+            else
+            {
+                Console.WriteLine("Invalid page");
+            }
+        }
+
+        public override string ToString()
+        {
+            return title + " by " + author + ", a " + coverStyle + " " + category + " book with " + pages + " page" + (pages == 1 ? "." : "s.") + " ISBN: " + ISBN;
+        }
+
+    }
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
             string input = "";
+
+
+
+
+
 
             Console.WriteLine("Welcome to the Simple Menu Demonstration Application");
             Console.WriteLine("====================================================");
@@ -21,6 +96,7 @@ namespace pos409_sandbox
                 Console.WriteLine("=========");
                 Console.WriteLine("1: Strings");
                 Console.WriteLine("2: Lottery");
+                Console.WriteLine("3: Structs");
                 Console.WriteLine("X: Exit");
 
                 input = Console.ReadLine();
@@ -66,6 +142,30 @@ namespace pos409_sandbox
 
 
                 }
+
+                else if (input.Equals("3"))
+                {
+                    bookStruct myBook = new bookStruct("Design Patterns: Elements of Reusable Object-Oriented Software", "Software Engineering",
+                                                        "Gang of Four", 395, "0-201-63361-2", "Paperback");
+                    Console.WriteLine(myBook);
+                    myBook.prevPage();
+                    myBook.nextPage();
+                    myBook.nextPage();
+                    myBook.setPage(400);
+                    myBook.setPage(395);
+                    myBook.nextPage();
+
+                    Console.WriteLine("========");
+
+                    bookStruct nullBook = new bookStruct("Nullinomicon", "Errors", "void", 0, "0000", "Vacuum");
+                    Console.WriteLine(nullBook);
+                    nullBook.prevPage();
+                    nullBook.nextPage();
+
+
+                }
+
+
                 else if (input.Equals("X"))
                 {
                     Console.WriteLine("Goodbye.");
